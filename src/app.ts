61a89059644cbd7304cc271277ac8b7f9a1878a4
Flávio <f.augustosdn@gmail.com>
2025-01-07 23:10:06 -0300

    feat: implementado tratamento de exceções

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,12 @@
-import express from 'express'
+import 'express-async-errors'
+import express, { Request, Response, NextFunction } from 'express'
+import { errorHandling } from './middlewares/errorHandling'
 
 export const app = express()
 app.use(express.json())
+
+app.use(
+    (error: any, request: Request, response: Response, next: NextFunction) => {
+        errorHandling(error, request, response, next)
+    },
+)
