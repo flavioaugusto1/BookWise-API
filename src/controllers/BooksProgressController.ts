99fd267e844ebe033eecc4117453fb5568a8d4b3
Flávio <f.augustosdn@gmail.com>
2025-01-15 00:52:38 -0300

    feat: retornar progresso de um livro

---
+++ src/controllers/BooksProgressController.ts
@@ -0,0 +1,90 @@
+import { prisma } from '@/database/prisma'
+import { AppError } from '@/utils/AppError'
+import { Request, Response } from 'express'
+import { z } from 'zod'
+
+export class BooksProgressController {
+    async update(request: Request, response: Response) {
+        const requestBodySchema = z.object({
+            readedPages: z.number().int().min(0),
+        })
+
+        const requestParamSchema = z.object({
+            id: z.string().uuid(),
+        })
+
+        const { readedPages } = requestBodySchema.parse(request.body)
+        const { id } = requestParamSchema.parse(request.params)
+
+        const book = await prisma.book.findFirst({
+            where: {
+                id,
+            },
+        })
+
+        if (!book) {
+            throw new AppError('O livro informado não existe', 404)
+        }
+
+        const { id: user_id } = request.user
+
+        if (book.user_id !== user_id) {
+            throw new AppError('Você não possui esse livro cadastrado', 404)
+        }
+
+        if (readedPages > book.numberOfPages) {
+            throw new AppError(
+                'O número de páginas lidas está maior que o número de páginas do livro.',
+                409,
+            )
+        }
+
+        await prisma.book.update({
+            data: {
+                readedPages,
+            },
+            where: {
+                id,
+            },
+        })
+
+        response.json({ message: 'Número de páginas atualizadas com sucesso!' })
+    }
+
+    async show(request: Request, response: Response) {
+        const requestParamSchema = z.object({
+            id: z.string().uuid(),
+        })
+
+        const { id } = requestParamSchema.parse(request.params)
+
+        const book = await prisma.book.findFirst({
+            where: {
+                id,
+            },
+        })
+
+        if (!book) {
+            throw new AppError('O livro informado não existe', 404)
+        }
+
+        const { id: user_id } = request.user
+
+        if (book.user_id !== user_id) {
+            throw new AppError('Você não possui esse livro cadastrado', 404)
+        }
+
+        const { readedPages, numberOfPages } = book
+
+        const percentBookReaded = Number(
+            ((readedPages / numberOfPages) * 100).toFixed(2),
+        )
+
+        response.json({
+            total_lido: readedPages,
+            total_paginas: numberOfPages,
+            porcentagem_lida: percentBookReaded,
+            message: `Você leu ${readedPages} páginas de ${numberOfPages}. Seu progresso no livro é de ${percentBookReaded}%.`,
+        })
+    }
+}
--- src/routes/books.routes.ts
+++ src/routes/books.routes.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express'
 import { BooksController } from '@/controllers/BooksController'
 import { ensureAuthenticated } from '@/middlewares/ensureAuthenticated'
+import { BooksProgressController } from '@/controllers/BooksProgressController'
 
 export const booksRoute = Router()
 const booksController = new BooksController()
+const booksProgressController = new BooksProgressController()
 
 booksRoute.use(ensureAuthenticated)
 booksRoute.post('/', booksController.create)
@@ -11,3 +13,6 @@ booksRoute.get('/', booksController.index)
 booksRoute.get('/:id', booksController.show)
 booksRoute.put('/:id', booksController.update)
 booksRoute.delete('/:id', booksController.delete)
+
+booksRoute.patch('/:id/progress', booksProgressController.update)
+booksRoute.get('/:id/progress', booksProgressController.show)
